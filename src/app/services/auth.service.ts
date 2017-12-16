99687086dc4d686f71b0cc756de6085e6f8c231c
Paul O'Connor <jamespauloconnor@gmail.com>
2017-12-16 15:35:42 -0500

    Module 7

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { RegisterUser } from '../models/RegisterUser';
-import { HttpClient } from '@angular/common/http/src/client';
+import { HttpClient } from '@angular/common/http';
+import { Token } from '../models/Token';
 
 const Api_Url = 'http://kcpelevennoteapie.azurewebsites.net';
 
@@ -10,6 +11,17 @@ export class AuthService {
   constructor( private _http: HttpClient) { }
 
   register(regUserData: RegisterUser){
-    return this._http.post(`${Api_Url}/api/Register`, regUserData);
+    return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
   }
+
+  login(loginInfo){
+    const str = 
+      `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
+
+    return this._http.post(`@{Api_Url}/token`, str).subscribe((token: Token) => {
+      localStorage.setItem('id_token', token.access_token);
+    });
+
+  }
+
 }
